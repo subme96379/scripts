7b5b6f224967fb783e093da5d0776b0eca9c4a01
James <37597405+zabtc@users.noreply.github.com>
2019-01-02 04:16:27 +0200

    Create haven.sh

---
+++ haven.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+cd ~
+cd /usr/local/bin
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
