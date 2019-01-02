6a7e0a98b61915920b19ffc324dc89b3175e180b
James <37597405+zabtc@users.noreply.github.com>
2019-01-02 03:04:44 +0200

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -8,4 +8,6 @@ cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
+sudo apt-get install tmux
+tmux
 sudo ./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 47K3DXiEFzAZTvpYaw6zwiSpt4pQuTZRnKqzLmZuoF2CUQRM9gdhc6yLo4PCJe6zgxfAsRuyeToPxGmndkr7rwsURvxwDBu -p x -t 4
