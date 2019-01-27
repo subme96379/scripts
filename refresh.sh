bc498c5b98d3a676e8ddfe2f53903aaff403ca02
subme96379 <42718117+subme96379@users.noreply.github.com>
2019-01-27 13:39:58 -0500

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -4,9 +4,9 @@ cd ~
 cd /usr/local/bin
 ./northern-cli stop
 rm -rf northernd northern-cli northern-tx
-wget https://github.com/zabtc/Northern/releases/download/2.2.0/northern-2.2.0-x86_64-linux-gnu.tar.gz
-tar -xzf northern-2.2.0-x86_64-linux-gnu.tar.gz
-rm -rf northern-2.2.0-x86_64-linux-gnu.tar.gz
+wget https://github.com/zabtc/Northern/releases/download/2.4.0/northern-2.4.0-x86_64-linux-gnu.tar.gz
+tar -xzf northern-4.2.0-x86_64-linux-gnu.tar.gz
+rm -rf northern-2.4.0-x86_64-linux-gnu.tar.gz
 ./northernd -daemon
 sleep 30
 ./northern-cli getinfo
